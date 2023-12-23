c2a51894b23e3c109b1b0c8d87ad060900118b61
habitcoach <the_habit_coach@outlook.com>
2023-12-23 15:52:47 +0530

    Add project files.

---
+++ cSharp_module_11/Program.cs
@@ -0,0 +1,86 @@
+﻿//using System;
+
+//namespace DynamicExample
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            dynamic d = 1; // type: dynamic {int}
+
+//            var testSum = d + 3; // type: dynamic {int}
+
+//            d = 2.0;
+//        type: dynamic { double}
+//            var i = 2; // type: int
+
+//            #region note
+//            DummyMethod(d) this wont throw any error because
+//             dynamic type will be resolved at run time and we can only get runtime error.
+//             But this wont be the case with int i as it is strongly typed
+//            #endregion
+
+//            DummyMethod(i);
+
+//                void DummyMethod(string a)
+//                {
+//                    Console.WriteLine(a);
+//                }
+//                #region notes
+
+//                /*
+//                 when you create dynamic object it wont get any  intellisense.  You can check it using dot
+//                use i. will give intellisence
+//                but d. wont as d in dynamic
+
+//                 */
+
+//                #endregion
+
+
+//                Console.WriteLine($"d: {d}, testSum: {testSum}, i: {i}");
+//                Console.WriteLine(s);
+
+
+
+//            }
+//        }
+//    }
+
+using System;
+using Microsoft.Office.Interop.Word;
+
+namespace DynamicCOMExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new Word application
+            dynamic wordApp = Activator.CreateInstance(Type.GetTypeFromProgID("Word.Application"));
+            wordApp.Visible = true;
+
+            // Add a new document
+            dynamic document = wordApp.Documents.Add();
+
+            // Write content to the document
+            dynamic paragraph = document.Paragraphs.Add();
+            paragraph.Range.Text = "Hello from dynamic COM! This is a new document.";
+
+            // Save the document on the D: drive
+            string filePath = @"D:\MyNewDocument.docx";
+            document.SaveAs(filePath);
+
+            // Close Word
+            wordApp.Quit();
+
+            Console.WriteLine("Document saved on D:. Press any key to exit.");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
+
+
---
+++ objectDisposeDemo/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student : IDisposable
+{
+    private bool disposed = false;
+
+    // Simulated managed resource
+    private List<string> courses;
+
+    // Simulated unmanaged resource
+    private IntPtr unmanagedResource;
+
+    public Student(string name)
+    {
+        courses = new List<string>();
+        unmanagedResource = IntPtr.Zero; // Simulated unmanaged resource allocation
+        Console.WriteLine($"{name} instance created.");
+    }
+
+    public void EnrollInCourse(string courseName)
+    {
+        courses.Add(courseName);
+        Console.WriteLine($"{courseName} enrolled.");
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this); //This method call informs the garbage collector that finalization (the execution of the finalizer, ~Student()) for the object is not necessary since the resources have already been properly released during explicit disposal.
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                // Release managed resources here
+                courses.Clear();
+                Console.WriteLine("Courses cleared.");
+            }
+
+            // Release unmanaged resources here
+            if (unmanagedResource != IntPtr.Zero) //unmanaged wont be collecred by garbage collector and better to manually dispose
+            {
+                // Simulated unmanaged resource release
+                Console.WriteLine("Unmanaged resources released.");
+                unmanagedResource = IntPtr.Zero;
+            }
+
+            disposed = true;
+        }
+    }
+
+    //if developer forgets to call dispose finalizer that is ~studetn will be called which will remove the 
+    //object.  And Dispose is set to false as managed data is already disposed it will dispose unmanaged data alone by calling dispose method
+    ~Student()
+    {
+        Console.WriteLine("Finalizer called.");
+        Dispose(false);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student("John");
+
+        // Enroll the student in some courses
+        student.EnrollInCourse("Math");
+        student.EnrollInCourse("History");
+
+        // Now, manually call Dispose to release resources
+        student.Dispose();
+
+        Console.WriteLine("Resources released manually.");
+    }
+}
+
+#region Program flow
+
+/*
+
+Firstly an instance of student is create will will create both managed and unmanaged object.
+And then student in enrolled which will add data to managed list.
+
+Now we are explicitly calling student.dispose which will call  public void Dispose() and where
+Dispose(true) is called with true and since disposed = false initially it will clean the managed and unmanaged
+object.  Now since object are release we can ask garbage collector not to run using GC.SuppressFinalize(this);
+
+Now let suppose developer is not calling dispose then garbage collector will be called that is  ~Student()
+and it will release the managed data.  Now it is going to call  Dispose(false) to release unmanaged data
+as unmanaged data is not released by the garbage collector
+
+
+ 
+ */
+
+#endregion
